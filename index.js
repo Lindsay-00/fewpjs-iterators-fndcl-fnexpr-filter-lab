9dedaaea2693e117e5f0bded80ac2b7b710d11d7
Lindsay <lindsay.chen@yale.edu>
2021-07-31 04:06:30 +0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter( match => match.toUpperCase() === str.toUpperCase())
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter( match => match.toUpperCase().slice(0, str.length) === str.toUpperCase())
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(match => match.name === str)
+}
